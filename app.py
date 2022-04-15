5a1fc2be2420049f3c197bc38688e6cd53f046c1
GOPIKAJAYARAM <96109158+GOPIKAJAYARAM@users.noreply.github.com>
2022-04-15 20:15:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,36 @@
 import os
 import dash
+# from jupyter_dash import JupyterDash
+import plotly.express as px
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.express as px
+import dash_auth
+from dash.dependencies import Input,Output 
 import pandas as pd
 
-
-app = dash.Dash(__name__)
-server = app.server
-
-df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
-
-fig = px.scatter(df, x="gdp per capita", y="life expectancy",
-                 size="population", color="continent", hover_name="country",
-                 log_x=True, size_max=60)
-
-app.layout = html.Div([
-    dcc.Graph(
-        id='life-exp-vs-gdp',
-        figure=fig
-    )
+USERNAME_PASSWORD_PAIRS=[["gopika","gopika"]]
+df=px.data.tips()
+# app=JupyterDash()
+app=dash.Dash(__name__)
+auth=dash.auth.BasicAuth(app,USERNAME_PASSWORD_PAIRS)
+server=app.server
+app.layout=html.Div([
+                     dcc.Dropdown(
+                         id="dropdown",
+                         options=[{"label":x,"value":x} for x in df.day],
+                         value=df.day[0],
+                         clearable=False,
+                     ),
+                     dcc.Graph(id="bar-chart"),
 ])
 
+@app.callback(
+    Output("bar-chart","figure"),
+    [Input("dropdown","value")])
+def update_bar_chart(day):
+  mask=df["day"]==day
+  fig=px.bar(df[mask],x="sex",y="total_bill",color="smoker",barmode="group")
+  return fig 
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+if __name__ == "__main__":
+  app.run_server(debug=True)
